fd0dcd875711c5b1b2fc19956b7c56709eb370f8
RealSteelK <realsteelkim95@gmail.com>
2025-05-05 00:44:54 +0900

    Applied Prism

--- WpfApp/WpfApp/App.xaml.cs
+++ WpfApp/WpfApp/App.xaml.cs
@@ -1,14 +1,27 @@
-﻿using System.Configuration;
+﻿using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Windows;
+using System.Windows.Media.Animation;
 
 namespace WpfApp
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
-    {
+    public partial class App : Prism.Unity.PrismApplication
+    { 
+        protected override Window CreateShell()
+        {
+            //Return your main window here
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            //Register your services and views here
+            containerRegistry.RegisterForNavigation<MainWindow>();
+        }
     }
 
 }
